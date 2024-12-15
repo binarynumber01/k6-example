fd0cf682a6436851e27a9789d76e8a09aa6974af
binarynumber01 <168270930+binarynumber01@users.noreply.github.com>
2024-12-15 20:25:19 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
-
 export const options = {
-  iterations: 10,
+  vus: 10,
+  duration: '30s',
 };
-
-// The default exported function is gonna be picked up by k6 as the entry point for the test script. It will be executed repeatedly in "iterations" for the whole duration of the test.
 export default function () {
-  // Make a GET request to the target URL
-  http.get('https://test-api.k6.io');
-
-  // Sleep for 1 second to simulate real-world usage
+  http.get('http://test.k6.io');
   sleep(1);
 }
